e516e5d2a35e965b61d20b047df2bfbb6278620f
Wanderson Andrade <wmandrade81@gmail.com>
2022-06-09 22:07:07 -0300

    Atividade SPA

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './components/contato/contato.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { QuemsomosComponent } from './components/quemsomos/quemsomos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'quemSomos', component: QuemsomosComponent},
+  {path: 'contato', component: ContatoComponent},
+  {path: 'produtos', component: ProdutosComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegationComponent } from './components/navegation/navegation.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
+import { ContatoComponent } from './components/contato/contato.component';
+import { QuemsomosComponent } from './components/quemsomos/quemsomos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegationComponent,
+    ProdutosComponent,
+    RodapeComponent,
+    ContatoComponent,
+    QuemsomosComponent
   ],
   imports: [
     BrowserModule,
